5cbb4658f0ede376b3c83c8622459ff3a12d87c0
DukeePacoca <miguel.burger@loyola.g12.br>
2025-08-14 15:00:58 -0300

    Create Aula 1.py

---
+++ Aula 1.py
@@ -0,0 +1,25 @@
+print('Hello World')
+
+nome = 'Miguel'
+idade = '12'
+turma = 'A'
+jogo = 'Brawl Stars'
+comida = 'Batata'
+país = 'Brasil'
+
+print('O meu nome é ' + nome)
+print('Eu tenho ' + str(idade) + ' anos')
+print('Eu sou da turma ' + (turma))
+print('Eu gosto de jogar ' + jogo)
+print('Eu gosto de ' + (comida))
+print('Eu moro no ' + (país))
+print()
+print()
+
+num1 = 10
+num2 = 5
+num3 = 20
+num4 = 15
+resultado = num1 + num2 + num3 + num4 
+resultado2 = (resultado) / 4
+print(resultado2)
